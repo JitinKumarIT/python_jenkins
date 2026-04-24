32bbc303a558c705bfd58073405cabba40dc67b1
JitinKumarIT <54325844+JitinKumarIT@users.noreply.github.com>
2026-04-24 23:29:37 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,14 @@
+import unittest
 from app import app
 
-def test_home():
-    client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Hello, CI/CD Pipeline!", response.data)
+
+if __name__ == '__main__':
+    unittest.main()
