7cbaa51392a5dbd09536948db73941f5ec81415e
JitinKumarIT <54325844+JitinKumarIT@users.noreply.github.com>
2026-04-25 16:48:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ def home():
     return "Hello, Jenkins CI/CD Pipeline!"
 
 if __name__ == "__main__":
-    # Tell Bandit to ignore the B104 warning for this specific line
-    app.run(host="0.0.0.0", port=5000)  # nosec B104
+    app.run(host="0.0.0.0", port=5000)
